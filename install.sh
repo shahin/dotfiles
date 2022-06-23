7dd7f8eba903518088c863f61b5546e407add15b
Shahin Saneinejad <shahin.saneinejad@discordapp.com>
2022-06-23 09:50:40 -0700

    Add starship prompt and homeshick dotfiles installation

--- install.sh
+++ install.sh
@@ -3,4 +3,15 @@
 set -euo pipefail
 set -x
 
+# install dotfiles manager
 git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+
+# install starship prompt
+curl -sS https://starship.rs/install.sh >install-starship.sh && \
+    chmod 755 install-starship.sh && \
+    ./install-starship.sh --yes
+
+# add dotfiles
+source "$HOME/.homesick/repos/homeshick/homeshick.sh"
+homeshick clone --batch shahin/dotfiles
+homeshick link dotfiles
